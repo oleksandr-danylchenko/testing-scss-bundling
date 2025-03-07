757d6465f10d076ebdcc8bfde4591a1caeeab927
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2025-03-07 19:16:57 +0200

    Added colors file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import exportedValue from './styles.scss';
-console.log(exportedValue);
+import colors from './colors.scss';
+console.log(colors);
 
 const app = document.getElementById('app');
-app.innerHTML = '<h1>Hello, Webpack with SCSS!</h1>';
+app.innerHTML = `<h1>Hello, I found those colors: ${JSON.stringify(colors)}</h1>`;
